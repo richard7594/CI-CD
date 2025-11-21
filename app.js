7d112d1bbcdb215babaf6ef7655c7b580bd52e67
RicharSlifer <104769174+richard7594@users.noreply.github.com>
2025-11-21 20:32:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello CI/CD! author :=>+++++++++Richardslifer+++++++ 10/10/25 thanks'));
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(3001, () => console.log('App running on port 3000'));
 
